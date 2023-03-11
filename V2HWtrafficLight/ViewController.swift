5e466f110f03263c49622c4b3b70534be556236c
Oleksii <105242112+OleksiiHolub@users.noreply.github.com>
2023-03-11 22:31:58 +0200

    Initial Commit

---
+++ V2HWtrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  V2HWtrafficLight
+//
+//  Created by Oleksii Holub on 11.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
